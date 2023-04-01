5453938cffc6d331e48df8be546ac7e4cb552bac
Felix Bjært Hargreaves <felix.palludan.hargreaves@lego.com>
2023-04-01 13:36:21 +0200

    ping fixes

--- modules/examples/js/src/main/scala/fennec/examples/PingApp.scala
+++ modules/examples/js/src/main/scala/fennec/examples/PingApp.scala
@@ -12,6 +12,7 @@ import fennec.KernelCatsSupport.given
 import fennec.UpdateEffect
 import fs2.concurrent.Topic
 import fs2.dom.HtmlElement
+import cats.effect.syntax.all.*
 
 import scala.annotation.nowarn
 
@@ -23,18 +24,33 @@ class PingApp[F[_] : Async : Dispatcher : UUIDGen : Logger : LocalStorage](
 
   import html.{given, *}
 
+  private val explanation =
+    s"""
+      |This demo measures the overhead in terms of network and computation per message round-trip.
+      |A ping-pong between the client and server is measured over $iterations iterations.
+      |You can check the dev-tools to see the messages going back and forth. To not interfere
+      |with the measurement too much, nothing updates in the dom before the test is finished.
+      |""".stripMargin
 
   private val buttonClass =
-    cls := "m-2 transition bg-blue-500 hover:bg-green-500 text-white font-bold py-2 px-4 rounded-full hover:scale-150 disabled:opacity-25"
-
+    cls := "m-2 transition bg-blue-500 text-white font-bold py-2 px-4 rounded-full hover:scale-120 hover:disabled:scale-100 disabled:opacity-25 disabled:animate-pulse"
 
   override def render(outgoing: Topic[F, Event], states: fs2.Stream[F, State]): Resource[F, HtmlElement[F]] =
+    for
+      _ <- outgoing.publish1(Event.Reset).toResource
+      html <- renderApp(outgoing, states)
+    yield html
+
+  private def renderApp(outgoing: Topic[F, Event], states: fs2.Stream[F, State]) = {
     div(
+      p(cls := "font-mono text-lg",explanation),
+      br(()),
       button("start", buttonClass, onClick.as(Event.Start) --> outgoing.publish, disabled <-- states.map(_.isStarted).holdResource(false)),
       br(()),
       states.changesBy(_.isStarted).map(state => p(s"Is started?: ${state.isStarted}")).holdOptionResource,
       states.changesBy(_.stoppedEpochMillis).map(s => div(s"Roundtrip time based on ${iterations} iterations: ${calculatePing(s).getOrElse("N/A")} milliseconds")).holdOptionResource
     )
+  }
 
   def calculatePing(s: State): Option[Double] = s match
     case State(_, Some(startedMillis), Some(stoppedMillis), _) => Some((stoppedMillis - startedMillis).toDouble / iterations)
--- modules/examples/shared/src/main/scala/fennec/examples/PingKernel.scala
+++ modules/examples/shared/src/main/scala/fennec/examples/PingKernel.scala
@@ -13,6 +13,7 @@ object PingKernel:
   enum Event:
     case Start
     case Stop
+    case Reset
     case Started(epochMillis: Long)
     case Stopped(epochMillis: Long)
     case Ping
@@ -25,9 +26,10 @@ object PingKernel:
   val update: Update[State, Event] = state =>
     case Event.Ping => state.copy(count = state.count + 1)
     case Event.Start => state.copy(isStarted = true, count = 0)
-    case Event.Stop => state.copy(isStarted = false)
+    case Event.Stop => state.copy(isStarted = false, count = 0)
     case Event.Stopped(stoppedMillis) => state.copy(stoppedEpochMillis = Some(stoppedMillis))
     case Event.Started(startedMillis) => state.copy(startedEpochMillis = Some(startedMillis))
+    case Event.Reset => initialState
     case _ => state
 
   val initialState: State = State(count = 0, startedEpochMillis = None, stoppedEpochMillis = None, isStarted = false)
